ed17fb4fdc606a3b14d013c79d583ab6f7c472ae
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-07-26 19:55:37 +0200

    1.0.1
    - animated status

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,26 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
+const { serverID } = require('../config.json');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(client.user.tag);
         client.guilds.cache.forEach(async (g) => {
             await g.members.fetch({ cache: true });
             console.log(`Fetched members for ${g.name} (${g.memberCount})`);
         });
+        const channel = await client.channels.fetch('1124570199018967075');
+        const threads = await channel.threads.fetch();
+        const members = client.guilds.cache.get(serverID).members.cache.size;
+        let state = 0;
+        const presences = [
+            { name: `${members} members <3`, type: ActivityType.Watching },
+            { name: `${threads.threads.size} models!`, type: ActivityType.Watching }
+        ]
+        setInterval(() => {
+            state = (state + 1) % presences.length;
+            client.user.setPresence({ activities: [presences[state]], status: 'dnd' });
+        }, 5000);
     }
 }
