0ce8be255374d1a628cbcf84b798a1e77a3d748d
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-07-28 12:36:53 +0200

    1.0.8
    - new top command that displays leaderboard
    - member leave event small fix

---
+++ commands/info/top.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const memberdata = require('../../memberdata.json');
+
+module.exports = {
+    ephemeral: false,
+    data: new SlashCommandBuilder()
+    .setName('top')
+    .setDescription('Topka serwera w levelach.'),
+    async execute(interaction) {
+        let member = interaction.member;
+        let leaderboard = [];
+        memberdata.forEach(e => {
+            leaderboard.push({ id: e.id, xp: e.level.xp + (e.level.lvl * 150) });
+        });
+        leaderboard.sort((a, b) => b.xp - a.xp);
+
+        const executerplace = leaderboard.findIndex(e => e.id === member.user.id) + 1;
+
+        let desc = '';
+        for (let i = 0; i < leaderboard.length; i++) {
+            let mem = interaction.guild.members.cache.get(leaderboard[i].id);
+            let memd = memberdata.find(m => m.id === leaderboard[i].id)
+            desc += `\`${i + 1}.\` **${mem.user.username}** - ${memd.level.lvl} poziom (${memd.level.xp} XP)\n`;
+        }
+
+        const top = new EmbedBuilder()
+        .setTitle('AI Hub Polska Leaderboard')
+        .setDescription(desc)
+        .setColor('#ffffff')
+        .setThumbnail(interaction.guild.iconURL())
+        .setFooter({ iconURL: member.user.displayAvatarURL(), text: `${member.user.username}, Twoje miejsce: ${executerplace}` });
+
+        await interaction.followUp({ embeds: [top] });
+    }
+}
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,6 +4,6 @@ module.exports = {
     name: Events.GuildMemberRemove,
     async execute(member) {
         const general = await member.guild.channels.fetch('1124566636020633662');
-        general.send(`[-] ${member} <a:papa:1133797290209980466> (${member.guild.members.cache.size})`);
+        general.send(`[-] **${member.user.username}** <a:papa:1133797290209980466> (${member.guild.members.cache.size})`);
     }
 }
