b8ede36b10a7a259a5fec17a4d8458fc5c0ec5b0
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-07-28 14:36:49 +0200

    1.0.10
    - clear command

---
+++ commands/moderation/clear.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    ephemeral: true,
+    data: new SlashCommandBuilder()
+    .setName('clear')
+    .setDescription('Czyści kanał z wiadomości.')
+    .addIntegerOption(o => o
+        .setName('amount')
+        .setDescription('Podaj ilość wiadomości do wyczyszczenia (od 1 do 100)')
+        .setRequired(true)),
+    async execute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+        if (amount < 1 || amount > 100) return await interaction.followUp({ content: 'Wybierz liczbę wiadomości do usunięcia od 1 do 100!' });
+        const channel = interaction.channel;
+        const modchannel = await interaction.guild.channels.fetch('1134459462585942076');
+        await channel.bulkDelete(amount).then(async (m) => {
+            const log = new EmbedBuilder()
+            .setAuthor({ name: 'Usunięcie wiadomości', iconURL: 'https://i.imgur.com/hnWxPRI.png' })
+            .setDescription(`• Ilość usuniętych wiadomości: **${amount}**\n• Kanał: **${channel}**`)
+            .setFooter({ text: interaction.member.user.username, iconURL: interaction.member.user.displayAvatarURL() })
+            .setTimestamp(Date.now())
+            .setColor('#ffffff');
+            modchannel.send({ embeds: [log] });
+            await interaction.followUp({ content: `Pomyślnie usunięto **${amount}** wiadomości!` });
+        }).catch(console.error);
+    }
+}
