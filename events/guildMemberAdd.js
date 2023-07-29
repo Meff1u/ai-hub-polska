f1d77b2c34530a9a57e544dcf6b3045d6e3f4089
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-07-29 19:12:11 +0200

    1.0.13a
    - testing

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,14 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member) {
         const general = await member.guild.channels.fetch('1124566636020633662');
         general.send(`[+] ${member} <a:hi:1133797463623475230> (${member.guild.members.cache.size})`);
+        const modchannel = await interaction.guild.channels.fetch('1134459462585942076');
+        const newInvites = await member.guild.invites.fetch();
+        console.log(newInvites);
+        const log = new EmbedBuilder()
+        .setAuthor({ name: 'Dołączenie na serwer', iconURL: 'https://i.imgur.com/3lZDg9z.png' });
     }
 }
