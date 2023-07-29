2a163c71856938b6253400a414a9e968ce274aba
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-07-29 19:28:59 +0200

    1.0.13a-2
    - :|

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,7 +5,7 @@ module.exports = {
     async execute(member) {
         const general = await member.guild.channels.fetch('1124566636020633662');
         general.send(`[+] ${member} <a:hi:1133797463623475230> (${member.guild.members.cache.size})`);
-        const modchannel = await interaction.guild.channels.fetch('1134459462585942076');
+        const modchannel = await member.guild.channels.fetch('1134459462585942076');
         const newInvites = await member.guild.invites.fetch();
         console.log(newInvites);
         const log = new EmbedBuilder()
