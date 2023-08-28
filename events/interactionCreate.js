aba3f2a9f421366bca6fb84f08946035ea886d8d
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-08-28 10:46:23 +0200

    1.0.31
    - button and select menu for general information channel

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events, Collection, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+const { Events, Collection, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -64,6 +64,51 @@ module.exports = {
                 modal.addComponents(row);
                 await interaction.showModal(modal);
             }
+            else if (interaction.customId === 'info1') {
+                const e = new EmbedBuilder()
+                .setTitle('Oddzielanie wokali')
+                .setDescription('- [UVR](https://ultimatevocalremover.com/)\n- [X-MINUS](https://x-minus.pro/ai)\n- [Vocal Remover](https://vocalremover.org/)\n\nNajbardziej wydajnym i efektownym jest UVR, który z odpowiednimi ustawieniami robi dobrą robotę. Moderator <@183283323999617024> zrobił fajny tutorial do ustawień w UVR w [tej wiadomości](https://discord.com/channels/1124566634456174605/1124566636020633662/1137433709260312616)')
+                .setColor('#ff0000');
+                await interaction.reply({ embeds: [e], ephemeral: true });
+            }
+            else if (interaction.customId === 'info2') {
+                const e = new EmbedBuilder()
+                .setTitle('Tworzenie coverów/modeli - Strony')
+                .setDescription('- [EasyGUI na Google Colab](https://colab.research.google.com/drive/1r4IRL0UA7JEoZ0ZK8PKfMyTIBHKpyhcw)\n- [Kits.ai](https://www.kits.ai/)\n- [Voicemy.ai](https://www.voicemy.ai/)\n- Banger, aplikacja na [Android](https://play.google.com/store/apps/details?id=ai.cover.song.voicify) i [iOS](https://apps.apple.com/ae/app/id6452017015)')
+                .setColor('#ff0000');
+                await interaction.reply({ embeds: [e], ephemeral: true });
+            }
+            else if (interaction.customId === 'info3') {
+                const e = new EmbedBuilder()
+                .setTitle('Tworzenie coverów/modeli - Lokalnie')
+                .setDescription('- [Mangio-RVC](https://github.com/Mangio621/Mangio-RVC-Fork)\n- [VoiceConversionWebUI](https://huggingface.co/lj1995/VoiceConversionWebUI/tree/main)')
+                .setColor('#ff0000');
+                await interaction.reply({ embeds: [e], ephemeral: true });
+            }
+            else if (interaction.customId === 'info4') {
+                const e = new EmbedBuilder()
+                .setTitle('Poradniki')
+                .setDescription('- [RVC v2 AI Cover Guide (by kalomaze)](https://docs.google.com/document/d/13_l1bd1Osgz7qlAZn-zhklCbHpVRk6bYOuAuB78qmsE/edit?usp=sharing)\n- [Jak zrobić AI Cover (RVC2)](https://www.youtube.com/watch?v=8V4qI8ta_bU) (Film nagrany przez <@1026518790848122931>)\n- [Trenowanie modeli RVC v2 Poradnik](https://docs.google.com/document/d/1SQnkXCuEZw4ZO5voi1jxPXCwffvR3fNxYBbox2O5UaM/edit?usp=sharing) (by <@350691811280420864>)\n- [Tworzenie Coverów AI w RVC v2 Poradnik](https://docs.google.com/document/d/1mTO740MwvAdlzcmUMeviw7A_oo0ErLWDdRetLcAQ9Hw/edit?usp=sharing) (by <@350691811280420864>)')
+                .setColor('#ff0000');
+                await interaction.reply({ embeds: [e], ephemeral: true });
+            }
+            else if (interaction.customId === 'info5') {
+                const e = new EmbedBuilder()
+                .setTitle('FAQ')
+                .setDescription('Wybierz z poniższej listy pytanie, na które chcesz znać odpowiedź.')
+                .setColor('#ff0000');
+
+                const select = new StringSelectMenuBuilder().setCustomId('faq').setPlaceholder('Wybierz pytanie!').addOptions(
+                    new StringSelectMenuOptionBuilder().setLabel('Czym jest trenowanie modelu głosu?').setValue('pyt1'),
+                    new StringSelectMenuOptionBuilder().setLabel('Czy mogę podzielić się swoimi projektami?').setValue('pyt2'),
+                    new StringSelectMenuOptionBuilder().setLabel('Czy mogę poprosić o pomoc w rozwiązywaniu problemów?').setValue('pyt3'),
+                    new StringSelectMenuOptionBuilder().setLabel('Jak wytrenować swój model AI?').setValue('pyt4'),
+                    new StringSelectMenuOptionBuilder().setLabel('Jak zrobić swój cover AI?').setValue('pyt5'),
+                    new StringSelectMenuOptionBuilder().setLabel('Mój AI cover brzmi źle/nie jest czysty, dlaczego?').setValue('pyt6')
+                );
+                const row = new ActionRowBuilder().addComponents(select);
+                await interaction.reply({ embeds: [e], components: [row], ephemeral: true });
+            }
         }
         else if (interaction.isModalSubmit()) {
             if (interaction.customId === 'verify') {
@@ -74,6 +119,34 @@ module.exports = {
                 }
                 else {
                     await interaction.reply({ content: 'Nieprawidłowy wynik działania, spróbuj ponownie.', ephemeral: true });
+                }
+            }
+        }
+        else if (interaction.isChannelSelectMenu) {
+            if (interaction.customId === 'faq') {
+                if (interaction.values[0] === 'pyt1') {
+                    const e = new EmbedBuilder().setTitle('Czym jest trenowanie modelu głosu?').setDescription('Trenowanie głosów w modelach AI wykorzystuje algorytmy uczenia maszynowego do naśladowania i generowania głosów. Wytrenowany model głosu możemy wykorzystać do np. coveru jakiejś piosenki.').setColor('#ff0000');
+                    interaction.reply({ embeds: [e], ephemeral: true });
+                }
+                else if (interaction.values[0] === 'pyt2') {
+                    const e = new EmbedBuilder().setTitle('Czy mogę podzielić się swoimi projektami?').setDescription('Oczywiście! Zachęcamy do dzielenia się swoimi projektami i doświadczeniami. Jeśli chcesz się pochwalić utworzonym modelem głosowym utwórz post na <#1124570199018967075> (zgodnie z templatem). Jeśli chcesz się pochwalić utworzonym coverem, udostępnij go na <#1124738420405702837> lub <#1138199294822862858> (zgodnie z przeznaczeniem kanału).').setColor('#ff0000');
+                    interaction.reply({ embeds: [e], ephemeral: true });
+                }
+                else if (interaction.values[0] === 'pyt3') {
+                    const e = new EmbedBuilder().setTitle('Czy mogę poprosić o pomoc w rozwiązywaniu problemów?').setDescription('Tak, na tym serwerze znajdziesz społeczność gotową do pomocy. Możesz śmiało zadawać pytania i prosić o wsparcie w rozwiązywaniu problemów na kanale <#1133734121378680832>').setColor('#ff0000');
+                    interaction.reply({ embeds: [e], ephemeral: true });
+                }
+                else if (interaction.values[0] === 'pyt4') {
+                    const e = new EmbedBuilder().setTitle('Jak wytrenować swój model AI?').setDescription('Szczegółowy poradnik dot. trenowania modelu głosowego znajdziesz pod przyciskiem **Poradniki**.').setColor('#ff0000');
+                    interaction.reply({ embeds: [e], ephemeral: true });
+                }
+                else if (interaction.values[0] === 'pyt5') {
+                    const e = new EmbedBuilder().setTitle('Jak zrobić swój cover AI?').setDescription('Szczegółowy poradnik dot. tworzenia coveru AI znajdziesz pod przyciskiem **Poradniki**.').setColor('#ff0000');
+                    interaction.reply({ embeds: [e], ephemeral: true });
+                }
+                else if (interaction.values[0] === 'pyt6') {
+                    const e = new EmbedBuilder().setTitle('Mój AI cover brzmi źle/nie jest czysty, dlaczego?').setDescription('Rezultat covera jest zależny od wielu czynników i nie zawsze jest to twoja wina. Pamiętaj, aby twój cover brzmiał dobrze ważne jest, aby:\n- Wokal podany do konwersji nie miał żadnych intrumentali/dzwięków w tle/itp. Ponadto powinien on być czysty i mieć mało przerw.\n- Używać dobrego modelu głosu. Model, którego używasz być może jest niepełny lub po prostu nie radzi sobie z tego typu konwersją.\n- Dobrać odpowiedni sposób konwersji i pitch. Rezultat może wyjść dobrze na defaultowych ustawieniach programu, ale nie zawsze. Upewnij się, że testowałeś różne sposoby konwersji (harvest, crepe, mangio-crepe, rmvpe itp.) i pitchu (dodawanie lub odejmowanie jego wartości).').setColor('#ff0000');
+                    interaction.reply({ embeds: [e], ephemeral: true });
                 }
             }
         }
--- events/ready.js
+++ events/ready.js
@@ -31,7 +31,23 @@ module.exports = {
                 ginvites.each(ginvite => {
                     client.invites[ginvite.code] = ginvite.uses;
                 })
-            })
-        })
+            });
+        });
+
+        /*const m = await client.channels.cache.get('1145622047721017354').messages.fetch('1145622463003246703');
+        const e = new EmbedBuilder()
+            .setTitle('Strefa Informacji - wszystko co potrzebne.')
+            .setDescription('Poniżej znajdziesz przyciski, które zawierają najpotrzebniejsze wam informacje. Kliknij na konkretny, aby otrzymać więcej informacji.\n\n**Oddzielanie wokali** - Linki do stron/programów umożliwiające w łatwy sposób oddzielenie wokalu od instrumentalu.\n**Tworzenie coverów/modeli - Strony** - Linki do stron umożliwiających tworzenie coverów/modeli przez przeglądarke (dla użytkowników o słabym sprzęcie)\n**Tworzenie coverów/modeli - Lokalnie** - Linki do repozytoriów RVC do lokalnego tworzenia coverów i modeli. (Dla użytkowników o dobrym sprzęcie)\n**Poradniki** - Linki do poradników dot. tworzenia coverów i modeli za pomocą AI.\n**FAQ** - Najczęściej zadawane pytania.\n**Oficjalne AI Hub** - Link na oficjalny serwer discord AI Hub.')
+            .setColor('#ff0000')
+            .setThumbnail(m.guild.iconURL());
+        const b1 = new ButtonBuilder().setCustomId('info1').setLabel('Oddzielanie wokali').setStyle(ButtonStyle.Success);
+        const b2 = new ButtonBuilder().setCustomId('info2').setLabel('Tworzenie coverów/modeli - Strony').setStyle(ButtonStyle.Success);
+        const b3 = new ButtonBuilder().setCustomId('info3').setLabel('Tworzenie coverów/modeli - Lokalnie').setStyle(ButtonStyle.Success);
+        const b4 = new ButtonBuilder().setCustomId('info4').setLabel('Poradniki').setStyle(ButtonStyle.Success);
+        const b5 = new ButtonBuilder().setCustomId('info5').setLabel('FAQ').setStyle(ButtonStyle.Success);
+        const b6 = new ButtonBuilder().setLabel('Oficjalne AI Hub').setStyle(ButtonStyle.Link).setURL('https://discord.gg/aihub');
+        const row1 = new ActionRowBuilder().addComponents(b1, b2, b3, b4, b5);
+        const row2 = new ActionRowBuilder().addComponents(b6);
+        await m.edit({ content: '', embeds: [e], components: [row1, row2] });*/
     }
 }
