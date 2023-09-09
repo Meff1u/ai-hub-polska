e6f4ba3b9f12d8bfd3644edbe4adcaaa35b1c95e
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-09-09 13:17:10 +0200

    1.0.34c

--- commands/misc/download.js
+++ commands/misc/download.js
@@ -1,6 +1,14 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const Spotify = require('spottydl');
+const MusicDownloader = require("music-dwnldr");
 const fs = require('node:fs');
+const { spotify } = require('../../config.json');
+let credentials = require({
+    "clientId": spotify.ID,
+    "clientSecret": spotify.secret
+});
+
+let downloader = new MusicDownloader(credentials, __dirname);
 
 module.exports = {
     id: '1133857689764634725',
@@ -19,7 +27,15 @@ module.exports = {
         const url = interaction.options.getString('spotify_url');
         
         try {
-            const track = await Spotify.getTrack(url);
+            downloader.then((object) => {
+                downloader = object;
+                let download = downloader.downloadFromUrl(url).then((data) => {
+                    console.log(data);
+                }, (err) => {
+                    console.error(err);
+                });
+            }, (err) => console.error(err));
+            /*const track = await Spotify.getTrack(url);
             if (track.toString().includes('Error')) return await interaction.editReply('Błąd, upewnij się, że dałeś link do __utworu spotify__, nie albumu lub playlisty, czy innej platformy.');
             const title = `${track.artist} - ${track.title}`;
             await interaction.followUp(`Pobieranie **${title}**...`);
@@ -40,7 +56,7 @@ module.exports = {
             catch (err) {
                 console.err(err);
                 return await interaction.followUp({ content: 'WTF coś się odjebało.'});
-            }
+            }*/
         } catch (err) {
             console.log(err);
             return await interaction.followUp({ content: 'Link musi być do __utworu spotify__, nie albumu lub playlisty.\n(jeśli jednak dałeś link do utworu, oznacza to, że to jakiś poważny błąd i zgłoś to administratorowi, dzięki)' });
