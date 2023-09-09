79d0c8677b9e2ec84e256d1b06674033b0bb3c9c
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2023-09-09 13:01:07 +0200

    1.0.34a

--- commands/misc/download.js
+++ commands/misc/download.js
@@ -23,17 +23,23 @@ module.exports = {
             if (track.toString().includes('Error')) return await interaction.editReply('Błąd, upewnij się, że dałeś link do __utworu spotify__, nie albumu lub playlisty, czy innej platformy.');
             const title = `${track.artist} - ${track.title}`;
             await interaction.followUp(`Pobieranie **${title}**...`);
-            await Spotify.downloadTrack(track, './tracks').then(async r => {
-                if (r[0].status === 'Success') {
-                    const file = new AttachmentBuilder(r[0].filename, { name: `${title}.mp3` });
-                    await interaction.editReply({ content: 'Pobrano.', files: [file] });
-                    fs.unlinkSync(r[0].filename, (err) => {
-                        if (err) {
-                            throw err;
-                        }
-                    });
-                }
-            });
+            try {
+                await Spotify.downloadTrack(track, './tracks').then(async r => {
+                    if (r[0].status === 'Success') {
+                        const file = new AttachmentBuilder(r[0].filename, { name: `${title}.mp3` });
+                        await interaction.editReply({ content: 'Pobrano.', files: [file] });
+                        fs.unlinkSync(r[0].filename, (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                    }
+                });
+            }
+            catch (err) {
+                console.err(err);
+                return await interaction.followUp({ content: 'WTF coś się odjebało.'});
+            }
         } catch (err) {
             console.log(err);
             return await interaction.followUp({ content: 'Link musi być do __utworu spotify__, nie albumu lub playlisty.\n(jeśli jednak dałeś link do utworu, oznacza to, że to jakiś poważny błąd i zgłoś to administratorowi, dzięki)' });
